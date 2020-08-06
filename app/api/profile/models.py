d036b6d2419ce4233eff1dddbe62a5ed5747b8af
Owner <owner@localhost.owner>
2020-08-06 21:29:39 +0300

    Feature: was implemented profile registration

--- app/api/profile/models.py
+++ app/api/profile/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -6,16 +7,20 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Profile(AbstractUser):
+    def __str__(self):
+        return self.full_name
+
     class UserType(models.IntegerChoices):
         CLIENT = 1
         MASTER = 2
 
     user_type = models.IntegerField(choices=UserType.choices, default=UserType.CLIENT)
     birth_date = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=15, blank=True)
 
     @property
     def full_name(self):
-        return self.first_name + ' ' + self.last_name if self.first_name and self.last_name else ' '
+        return self.first_name + ' ' + self.last_name
 
     class Meta:
         verbose_name = 'Profile'
--- app/api/profile/serializers.py
+++ app/api/profile/serializers.py
@@ -1,15 +1,78 @@
+import re
+from datetime import datetime
+
+from django.utils.formats import date_format
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
+
 from .models import Profile
 
 
 class ProfileListSerializer(serializers.ModelSerializer):
+    birth_date = serializers.SerializerMethodField()
+    last_login = serializers.SerializerMethodField()
+    date_joined = serializers.SerializerMethodField()
+
     class Meta:
         model = Profile
         fields = ('username', 'first_name', 'last_name', 'birth_date', 'email',
-                 'last_login', 'is_active', 'date_joined')
+                  'last_login', 'is_active', 'date_joined')
+
+    def get_birth_date(self, obj):
+        return date_format(obj.birth_date, 'DATE_FORMAT') if obj.birth_date else ''
+
+    def get_last_login(self, obj):
+        return obj.last_login.strftime('%Y-%m-%d %H:%M:%S') if isinstance(obj.last_login, datetime) else ''
+
+    def get_date_joined(self, obj):
+        return obj.date_joined.strftime('%Y-%m-%d %H:%M:%S')
 
 
 class ProfileDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('id', 'first_name', 'last_name')
+
+
+class CreateProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('first_name', 'last_name', 'birth_date', 'email', 'phone', 'password')
+        extra_kwargs = {
+            'first_name': {'allow_blank': False, 'required': True},
+            'last_name': {'allow_blank': False, 'required': True},
+            'email': {
+                'required': True,
+                'min_length': 8,
+                'validators': [UniqueValidator(queryset=Profile.objects.all())]
+            },
+            'phone': {'allow_blank': False},
+            'password': {'required': True}
+        }
+        validators = [UniqueTogetherValidator(
+            queryset=Profile.objects.all(),
+            fields=('first_name', 'last_name'),
+        )]
+
+    def validate_first_name(self, value):
+        if any(map(str.isdigit, value)):
+            raise serializers.ValidationError('The first name contains numbers')
+        return value
+
+    def validate_last_name(self, value):
+        if any(map(str.isdigit, value)):
+            raise serializers.ValidationError('The last name contains numbers')
+        return value
+
+    def validate_birth_date(self, value):
+        if value >= datetime.now().date():
+            raise serializers.ValidationError('Sorry, we think you\'re too young for that sh**')
+        return value
+
+    def validate_phone(self, value):
+        if re.match(r'^\+\d{12}$', value) is None:
+            raise serializers.ValidationError('Incorrect phone number')
+        return value
+
+    def create(self, validated_data):
+        return Profile.objects.create_user(**validated_data, username=validated_data['email'])

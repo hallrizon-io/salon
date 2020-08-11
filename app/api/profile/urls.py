85f6832c25698d711ddb0924ec00414ffc9a5b0b
Owner <owner@localhost.owner>
2020-08-11 17:50:09 +0300

    Feature: made some changes

--- app/api/profile/urls.py
+++ app/api/profile/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import ProfileAPIView
-from ..master.views import MasterAPIView
+from api.master.views import CreateMasterView
 
 urlpatterns = [
     path('profiles/', ProfileAPIView.as_view()),
-    path('profiles/master', MasterAPIView.as_view()),
+    path('profiles/master', CreateMasterView.as_view()),
 ]

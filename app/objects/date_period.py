1b1348de9d597ac52e9bcf4438878c33f0454376
Owner <owner@localhost.owner>
2020-09-20 16:48:41 +0300

    Feature: was added type hints

--- app/objects/date_period.py
+++ app/objects/date_period.py
@@ -1,28 +1,34 @@
 from abc import abstractmethod, ABC
 from datetime import date, timedelta, datetime
+
+from django.contrib import admin
+
 from objects.calendar import Calendar
+from typing import List, final
 
 
-class DatePeriodState:
-    def __init__(self):
-        self.today = date.today()
+class DatePeriodState(ABC):
+    today = datetime.today()
 
     @property
     def context(self):
+        """Pointer on context"""
         return self._context
 
     @context.setter
-    def context(self, value):
+    def context(self, value: admin.SimpleListFilter):
+        """Set pointer on context"""
         self._context = value
 
     @abstractmethod
-    def get_dates(self):
+    def get_dates(self) -> List[datetime]:
+        """Return list of two date instances that based on state"""
         pass
 
     @staticmethod
-    def get_object(period):
-        if period is None:
-            period = 'default'
+    @final
+    def get_object(period: str) -> 'DatePeriodState':
+        """Return child instance of DatePeriodState"""
 
         periods = {
             'yesterday': YesterdayState(),
@@ -37,54 +43,57 @@ class DatePeriodState:
             'default': DefaultPeriod()
         }
 
+        if period not in periods:
+            period = 'default'
+
         return periods[period]
 
 
-class TodayState(DatePeriodState, ABC):
+class TodayState(DatePeriodState):
     def get_dates(self):
         return [self.today, self.today + timedelta(days=1)]
 
 
-class YesterdayState(DatePeriodState, ABC):
+class YesterdayState(DatePeriodState):
     def get_dates(self):
         return [self.today, self.today - timedelta(days=1)]
 
 
-class TomorrowState(DatePeriodState, ABC):
+class TomorrowState(DatePeriodState):
     def get_dates(self):
         return [self.today + timedelta(days=1), self.today + timedelta(days=2)]
 
 
-class PreviousWeek(DatePeriodState, ABC):
+class PreviousWeek(DatePeriodState):
     def get_dates(self):
         return Calendar.get_week_dates(self.today.year, self.today.isocalendar()[1] - 1)
 
 
-class ThisWeek(DatePeriodState, ABC):
+class ThisWeek(DatePeriodState):
     def get_dates(self):
         return Calendar.get_week_dates(self.today.year, self.today.isocalendar()[1])
 
 
-class NextWeek(DatePeriodState, ABC):
+class NextWeek(DatePeriodState):
     def get_dates(self):
         return Calendar.get_week_dates(self.today.year, self.today.isocalendar()[1] + 1)
 
 
-class PreviousMonth(DatePeriodState, ABC):
+class PreviousMonth(DatePeriodState):
     def get_dates(self):
         return Calendar.get_month_dates(self.today.year, self.today.month - 1)
 
 
-class ThisMonth(DatePeriodState, ABC):
+class ThisMonth(DatePeriodState):
     def get_dates(self):
         return Calendar.get_month_dates(self.today.year, self.today.month)
 
 
-class NextMonth(DatePeriodState, ABC):
+class NextMonth(DatePeriodState):
     def get_dates(self):
         return Calendar.get_month_dates(self.today.year, self.today.month + 1)
 
 
-class DefaultPeriod(DatePeriodState, ABC):
+class DefaultPeriod(DatePeriodState):
     def get_dates(self):
         return [datetime(1970, 1, 1), datetime(2038, 1, 19)]

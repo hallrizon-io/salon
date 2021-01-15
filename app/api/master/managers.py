40a90a3afc0c4c18473f8d56302281b14db337ed
kmink <kmin@softserveinc.com>
2021-01-15 15:23:52 +0200

    # CreateMasterView permission

--- app/api/master/managers.py
+++ app/api/master/managers.py
@@ -6,8 +6,11 @@ class MasterViewManager:
     def __init__(self, request):
         self.data = request.data
         self.context = {'new_company': self.data.get('new_company', '')}
-        if 'enter_code' in self.data.get('company'):
-            self.context['enter_code'] = self.data.get('company').get('enter_code')
+        try:
+            if 'enter_code' in self.data.get('company'):
+                self.context['enter_code'] = self.data.get('company').get('enter_code')
+        except:
+            pass
 
     def processing(self):
         master_serializer = CreateMasterSerializer(data=self.data, context=self.context)
